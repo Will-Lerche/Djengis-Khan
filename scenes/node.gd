647b2ed5cb36a5ad1091a7ed84f4557ceaaf51af
Uloea39 <magnuszx2@gmail.com>
2024-04-15 10:39:57 +0200

    hansbørge

---
+++ scenes/node.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+var enemy_scene = preload("res://scenes/Enemies.tscn")
+func _ready():
+	$Timer.start()
+
+func _on_Timer_timeout():
+	var enemy = enemy_scene.instance() 
+	add_child(enemy)
+	print("1")
--- scripts/player.gd
+++ scripts/player.gd
@@ -13,7 +13,7 @@ const BULLET_DAMAGE = 1
 func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
 		shoot()
-	$Node2D.look_at(get_global_mouse_position())
+	$Node2D2.look_at(get_global_mouse_position())
 	handle_input()
 func shoot():
 	var bullet = preload("res://scenes/Projectiles.tscn").instance()
