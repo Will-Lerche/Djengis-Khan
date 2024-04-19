46bebdaad534974aac74ee748f2b0ece668ed326
Emil-Gotlieb <emilgotlieb0@gmail.com>
2024-04-19 09:41:34 +0200

    buttons virker

--- scenes/Startscreen.gd
+++ scenes/Startscreen.gd
@@ -7,5 +7,6 @@ func _on_Start_pressed():
 	get_tree().change_scene_to(load("res://scenes/Level 1.tscn"))
 
 
+
 func _on_Controls_pressed():
-	pass
+	get_tree().change_scene_to(load("res://scenes/ControlsScene.tscn"))
