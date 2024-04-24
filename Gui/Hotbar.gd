0a9bb7d006878b4965272081b6641f97b5208fb1
Emil-Gotlieb <emilgotlieb0@gmail.com>
2024-04-24 13:28:13 +0200

    ændring til switch cases

--- Gui/Hotbar.gd
+++ Gui/Hotbar.gd
@@ -9,16 +9,13 @@ func _ready():
 func _input(event):
 	if event is InputEventKey: 
 		if event.pressed:
-			if Input.is_action_pressed("Hotbar_slot_1"):
-				switch_to_slot(0)
-				get_parent().get_parent().get_node("Player").get_node("Djengis").play("Bue skift")
-				
-			elif Input.is_action_pressed("Hotbar_slot_2"):
-				switch_to_slot(1)
-				get_parent().get_parent().get_node("Player").get_node("Djengis").play("Melee skift")
-			
-			elif Input.is_action_pressed("Hotbar_slot_3"):
-				switch_to_slot(2)
+			match event.scancode:
+				KEY_1:
+					switch_to_slot(0)
+					get_parent().get_parent().get_node("Player").get_node("Djengis").play("Bue skift")
+				KEY_2:
+					switch_to_slot(1)
+					get_parent().get_parent().get_node("Player").get_node("Djengis").play("Melee skift")
 
 func switch_to_slot(slot_index):
 	if slot_index != current_slot:
